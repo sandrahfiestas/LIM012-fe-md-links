94551332af18c36a8e5847d1a15bd4d775b271d4
sandrahfiestas <shfiestas@gmail.com>
2020-06-11 00:15:26 -0500

    Crear función que valide si la ruta es a un directorio
    Resuelve: #8

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-
+/* eslint-disable max-len */
 const path = require('path');
 const fs = require('fs');
+const clc = require('cli-color');
+// const colors = require('colors');
 
 // Determinar si la ruta es absoluta
 const pathIsAbsolute = (namePath) => path.isAbsolute(namePath);
@@ -12,19 +14,27 @@ const convertToAbsolute = (namePath) => (pathIsAbsolute(namePath) ? namePath : p
 // Determinar si la ruta es a un archivo
 const isFile = (namePath) => fs.statSync(namePath).isFile();
 
+// Determina si la ruta es a un directorio
+const isDirectory = (namePath) => fs.statSync(namePath).isDirectory();
+console.log('¿es un directorio', isDirectory('D:\\LIM012-fe-md-links\\src'));
 
 
+// Probando paleta CLI
+const error = clc.red.bold;
+const warn = clc.yellow;
+const notice = clc.blue;
+console.log(error('¡Error!'));
+console.log(warn('Advertencia'));
+console.log(notice('Aviso'));
 
+// Probando paleta de COLORS
+console.log('¡Error!'.red);
+console.log('Advertencia'.yellow);
+console.log('Aviso'.blue);
 
 module.exports = {
   pathIsAbsolute,
   convertToAbsolute,
   isFile,
+  isDirectory,
 };
-
-
-
-
-
-
-
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,4 +1,4 @@
-const index = require ('../src/index.js');
+const index = require('../src/index.js');
 
 const pathAbsolute = 'D:\\LIM012-fe-md-links\\src';
 const pathRelative = './src';
@@ -6,34 +6,47 @@ const pathDirectory = 'D:\\LIM012-fe-md-links\\test';
 const pathFile = 'D:\\LIM012-fe-md-links\\README.md';
 
 
-
 // ¿La ruta es absoluta?
-describe('pathIsAbsolute',() => {
-    it('deberia retornar false si la ruta no es absoluta', () => {
-        expect(index.pathIsAbsolute(pathRelative)).toBe(false);
-    });
+describe('pathIsAbsolute', () => {
+  it('deberia retornar false si la ruta no es absoluta', () => {
+    expect(index.pathIsAbsolute(pathRelative)).toBe(false);
+  });
 
-    it('deberia retornar true si la ruta es absoluta', () => {
-        expect(index.pathIsAbsolute(pathAbsolute)).toBe(true);
-    });
+  it('deberia retornar true si la ruta es absoluta', () => {
+    expect(index.pathIsAbsolute(pathAbsolute)).toBe(true);
+  });
 });
 
 // Convertir en absoluta
 describe('convertToAbsolute()', () => {
-    it('debería retornar una ruta absoluta al ingresar una ruta relativa', () => {
-      expect(index.convertToAbsolute(pathRelative)).toBe(pathAbsolute);
-    });
+  it('deberia retornar una ruta absoluta al ingesar una ruta absoluta', () => {
+    expect(index.convertToAbsolute(pathAbsolute)).toBe(pathAbsolute);
+  });
+
+  it('debería retornar una ruta absoluta al ingresar una ruta relativa', () => {
+    expect(index.convertToAbsolute(pathRelative)).toBe(pathAbsolute);
+  });
 });
 
 // ¿Es un archivo?
 describe('isfile()', () => {
-    it('debería retornar true si la ruta es un archivo', () => {
-        expect(index.isFile(pathFile)).toBe(true);
-    });
+  it('debería retornar true si la ruta es un archivo', () => {
+    expect(index.isFile(pathFile)).toBe(true);
+  });
+
+  it('debería retornar false si la ruta no es un archivo', () => {
+    expect(index.isFile(pathDirectory)).toBe(false);
+  });
 });
 
-describe('isfile()', () => {
-    it('debería retornar false si la ruta no es un archivo', () => {
-        expect(index.isFile(pathDirectory)).toBe(false);
-    });
+
+// ¿Es un directorio?
+describe('isDirectory()', () => {
+  it('debería retornar true si la ruta es a un directorio', () => {
+    expect(index.isDirectory(pathDirectory)).toBe(true);
+  });
+
+  it('debería retornar false si la ruta no es un directorio', () => {
+    expect(index.isDirectory(pathFile)).toBe(false);
+  });
 });
